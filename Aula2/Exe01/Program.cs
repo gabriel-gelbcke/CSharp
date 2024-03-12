ff32b62e0d0017b1d6c9e77be293a82867035c94
g4breu <gabriel.gelbcke16@gmail.com>
2024-03-11 21:10:40 -0300

    new changes

---
+++ Aula2/Exe01/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program 
+{
+     static void Main()
+     {
+          int fodaseViado = 0;
+
+          Scanner pau = new Scanner(System.in);
+
+
+          Console.WriteLine("Escreva um numero: ");
+          Console.ReadLine("Username is: " + userName);
+
+     }
+}
