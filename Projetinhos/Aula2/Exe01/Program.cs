fd71a2d31afaa4f471b679b4ccdaa289265f660b
g4breu <gabriel.gelbcke16@gmail.com>
2024-03-18 20:43:59 -0300

    a

---
+++ Projetinhos/Aula2/Exe01/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more informationpornn
+
+String talvez;
+
+
+Console.WriteLine("Sim ou não?");
+talvez = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine("fodase kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+
+
+
+
+
